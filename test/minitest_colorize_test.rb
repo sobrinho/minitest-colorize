4e94fc6c4979a3a2d9acbce0c46a63d890285f1c
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-06-25 15:25:35 -0300

    More concise test names

--- test/minitest_colorize_test.rb
+++ test/minitest_colorize_test.rb
@@ -1,35 +1,35 @@
 require 'test_helper'
 
 class MiniTest::ColorizeTest < MiniTest::Unit::TestCase
-  def test_fail
+  def test_print_fail
     assert_output "\e[31mF\e[0m" do
       colorize = MiniTest::Colorize.new($stdout)
       colorize.print 'F'
     end
   end
 
-  def test_error
+  def test_print_error
     assert_output "\e[31mE\e[0m" do
       colorize = MiniTest::Colorize.new($stdout)
       colorize.print 'E'
     end
   end
 
-  def test_skip
+  def test_print_skip
     assert_output "\e[33mS\e[0m" do
       colorize = MiniTest::Colorize.new($stdout)
       colorize.print 'S'
     end
   end
 
-  def test_success
+  def test_print_success
     assert_output "\e[32m.\e[0m" do
       colorize = MiniTest::Colorize.new($stdout)
       colorize.print '.'
     end
   end
 
-  def test_others
+  def test_print
     assert_output "colorize" do
       colorize = MiniTest::Colorize.new($stdout)
       colorize.print 'colorize'
